a00311222fd9580c69dcb6af0affa8d8f53a7061
nitinravi <nitinravi18@gmail.com>
2025-06-16 01:39:38 +0530

    hash router and build setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom';
 import { TestProvider } from './context/TestContext';
 import LandingPage from './components/LandingPage';
 import TestUpload from './components/test/TestUpload';
